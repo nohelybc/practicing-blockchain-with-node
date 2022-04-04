cc74224747177610666cec3b780847ca20e52968
Nohely <nohely.badillo96@gmail.com>
2022-04-04 01:03:12 -0500

    Structure of _Blockchain_ is done

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,7 @@
 const Block = require("./block");
 
+const SHA256 = requiere("crypto-js/sha256");
+
 class Blockchain {
   constructor() {
       this.chain = [];
@@ -17,8 +19,50 @@ class Blockchain {
   addBlock(Block) {
       let self = this;
       return new Promise((resolve, reject) => {
-          
-      })
+          block.height = self.chain.length;
+          block.time = new Date().getTime().toString();
+
+          if (self.chain.length > 0) {
+              block.previousBlockHash = self.chain[self.chain.length - 1].hash;
+          }
+
+          let errors = await self.validateChain();
+
+          if(errors.length > 0) {
+              reject(new Error('The chain is not valid: ', errors));
+          }
+
+          block.hash = SHA256(JSON.stringify(block)).toString();
+          self.chain.push(block);
+          resolve(block);
+      });
+  }
+
+  validateChain() {
+      let self = this;
+      const errors = [];
+
+      return new Promise((resolve, reject) => {
+          self.chain.map(async (block) => {
+              try {
+                  let isValid = await block.validate();
+                  if (!isValid) {
+                      errors.push(new Error(`The block ${block.height} is not valid`));
+                  }
+              } catch (err) {
+                  errors.push(err);
+              }
+          });
+
+          resolve(errors);
+      });
+  }
+
+  print() {
+      let self = this;
+      for (let block of self.chain) {
+          console.log(block.toString());
+      }
   }
 }
 
